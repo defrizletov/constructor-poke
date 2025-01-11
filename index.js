ba139ba532b894361edb76fd12aa924902262e9a
defriz <91366636+defrizletov@users.noreply.github.com>
2025-01-11 05:23:51 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import * as THREE from './js/three.module.js';
+import * as THREE from './js/three.min.js';
 import { GLTFLoader } from './js/addons/GLTFLoader.js';
 import ConstructorPoke from './constructor.js';
 
